ce0f1583c17c56852700565052048a77bc59d264
MirThomasson <mirthomasson@gmail.com>
2022-07-27 11:53:04 -0400

    solid logo animation

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,13 +1,14 @@
 import './index.scss';
 import LogoS from '../../../assets/images/m-logo.png';
 import React from 'react';
+import 'animate.css';
 
 
 const Logo = () => {
     
     return (
         <div className="logo-container">
-            <img className="solid-logo" src={LogoS} alt="M" />
+            <img className="solid-logo animate__tada" src={LogoS} alt="M" />
         </div>
     )
 }
