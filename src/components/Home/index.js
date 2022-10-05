c2c4de6d9da4d536ee821b5b52e34234f44caa85
MirThomasson <mirthomasson@gmail.com>
2022-10-05 11:31:31 -0400

    titles

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -38,7 +38,7 @@ const Home = () => {
                 idx={22} />
                 </h1>
                 <h2>
-                    Frontend Developer
+                    Frontend Developer / Web Designer / Content Manager
                 </h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
